d2536b0218a0717d5a267555304188fb22af3a3a
MaxRev <cmthea32@gmail.com>
2022-01-05 17:26:13 +0200

    squashed tcp proxy with program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Text.Json;
+using System.Net;
+using System.Net.Sockets;
+
 
 namespace NetProxy
 {
@@ -19,20 +22,7 @@ namespace NetProxy
 
                 Task.WhenAll(configs.Select(c =>
                 {
-                    if (c.Value.protocol == "udp")
-                    {
-                        try
-                        {
-                            var proxy = new UdpProxy();
-                            return proxy.Start(c.Value.forwardIp, c.Value.forwardPort, c.Value.localPort, c.Value.localIp);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Failed to start {c.Key} : {ex.Message}");
-                            throw ex;
-                        }
-                    }
-                    else if (c.Value.protocol == "tcp")
+                    if (c.Value.protocol == "tcp")
                     {
                         try
                         {
@@ -69,9 +59,80 @@ namespace NetProxy
         public string localIp { get; set; }
         public string forwardIp { get; set; }
         public ushort forwardPort { get; set; }
-    }
-    interface IProxy
+    } 
+    class TcpProxy  
     {
-        Task Start(string remoteServerIp, ushort remoteServerPort, ushort localPort, string localIp = null);
+        public async Task Start(string remoteServerIp, ushort remoteServerPort, ushort localPort, string localIp)
+        {
+            IPAddress localIpAddress = string.IsNullOrEmpty(localIp) ? IPAddress.IPv6Any : IPAddress.Parse(localIp);
+            var server = new TcpListener(new IPEndPoint(localIpAddress, localPort));
+            server.Server.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+            server.Start();
+
+            Console.WriteLine($"TCP proxy started {localPort} -> {remoteServerIp}|{remoteServerPort}");
+            while (true)
+            { 
+                try
+                {
+                    var remoteClient = await server.AcceptTcpClientAsync();
+                    remoteClient.NoDelay = true;
+                    var ips = await Dns.GetHostAddressesAsync(remoteServerIp);
+
+                    new TcpClient(remoteClient, new IPEndPoint(ips.First(), remoteServerPort));
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex);
+                    Console.ResetColor();
+                }
+
+            }
+        }
+    }
+
+    class TcpClient
+    {
+        private System.Net.Sockets.TcpClient _remoteClient;
+        private IPEndPoint _clientEndpoint;
+        private IPEndPoint _remoteServer;
+        public System.Net.Sockets.TcpClient _client = new System.Net.Sockets.TcpClient();
+
+        public TcpClient(System.Net.Sockets.TcpClient remoteClient, IPEndPoint remoteServer)
+        {
+            _remoteClient = remoteClient;
+            _remoteServer = remoteServer;
+            _client.NoDelay = true;
+            _clientEndpoint = (IPEndPoint)_remoteClient.Client.RemoteEndPoint;
+            Console.WriteLine($"Established {_clientEndpoint} => {remoteServer}");
+            Run();
+        }
+
+
+        private void Run()
+        {
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    using (_remoteClient)
+                    using (_client)
+                    {
+                        await _client.ConnectAsync(_remoteServer.Address, _remoteServer.Port);
+                        var serverStream = _client.GetStream();
+                        var remoteStream = _remoteClient.GetStream();
+
+                        await Task.WhenAny(remoteStream.CopyToAsync(serverStream), serverStream.CopyToAsync(remoteStream));
+                    }
+                }
+                catch (Exception) { }
+                finally
+                {
+                    Console.WriteLine($"Closed {_clientEndpoint} => {_remoteServer}");
+                    _remoteClient = null;
+                }
+            });
+        }
     }
 }
